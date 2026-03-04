4738013afe41655d13073db0d4d095467289780d
Mohammed Tawheed <Tawheed30@users.noreply.github.com>
2026-03-04 11:56:35 +0530

    Initial Commit

---
+++ SecureIOSReference/SecureIOSReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureIOSReferenceApp.swift
+//  SecureIOSReference
+//
+//  Created by Mohammed Tawheed on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SecureIOSReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
